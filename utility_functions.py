d1ce55fec8d8952abb5ea56308bcb21a21970e47
BBandy <b_bandy@hotmail.com>
2024-11-09 20:01:29 -0500

    get gainers from prev close

--- utility_functions.py
+++ utility_functions.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine, Column, Integer, Float, Date, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, column_property, declarative_base
 import json
+import pyodbc
+import pandas as pd
 
 Base = declarative_base()
 
@@ -22,7 +24,7 @@ class Daily(Base):
     date = Column(String(10), primary_key=True)
     percent_gain = column_property(((high - open) / open) * 100)
 
-def top_gainers():
+def top_gainers_sqlalchemy():
     # Query the top 10 gainers (customize this as needed)
     gainers = session.query(
         Daily.date,
@@ -40,6 +42,51 @@ def top_gainers():
     data = [{"Date": stock.date, "symbol": stock.symbol, "percent_gain": stock.percent_gain} for stock in gainers]
     with open("static\\top_gainers.json", "w") as f:
         json.dump(data, f, indent=4)
+
+def get_connection():
+        cnxn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
+                        "Server=DESKTOP-BKHK1H3;"
+                        "Database=Stonks;"
+                        "Trusted_Connection=yes;")
+        return cnxn
+
+def get_results_df(sql, values=None):
+        cnxn = get_connection()
+        if values is not None:
+            data = pd.read_sql(sql, params=values,con=cnxn) 
+        else:
+             data = pd.read_sql(sql,con=cnxn) 
+        return data
+
+def top_gainers():
+    sql = '''
+SELECT daily.symbol as symbol
+	, daily.[date] as [Date]
+	, round((daily.[high] - daily2.[close])/daily2.[close]*100, 2) as percent_gain
+	--, daily.volume as Volume
+	--, daily.[open] as [Open]
+	--, daily.[high] as [High]
+	--, daily.[low] as [Low]
+	--, daily.[close] as [Close]
+FROM dbo.daily_temp AS daily
+join dbo.daily_temp as daily2
+	on daily2.symbol = daily.symbol
+	and daily2.symbol_count-1 = daily.symbol_count
+WHERE  daily.[open] > 0 
+	and daily.volume > 20*1000000
+	--and daily.[open] >= .20											-- open price
+	and ((daily.[high] + daily.[low]) / 2) * daily.volume >= 5000000	-- dollar volume
+	and (daily.[high] - daily2.[close])/daily2.[close] >= .2				-- pct gain to hod from open
+
+
+ORDER BY daily.[date] DESC
+, (daily.[high] - daily.[open])/daily.[open] DESC
+'''
+    results_df = get_results_df(sql)
+    results_json = results_df.to_json(orient="records", lines=False)
+    results_json_clean = json.loads(results_json)
+    with open("static\\top_gainers.json", "w") as f:
+        json.dump(results_json_clean, f, indent=4)
 
 if __name__ == '__main__':
     top_gainers()
